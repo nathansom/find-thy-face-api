261a576af6215fc706f569c35d068a58ec480fc6
nathansom <nathan.sa.official@gmail.com>
2021-12-26 20:18:37 -0800

    Updated the database

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ const PORT = process.env.PORT;
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+        //host : process.env.DB_HOST,
+      //user : process.env.DB_USER,
+      //password : process.env.DB_PASS,
+      //database : process.env.DB_NAME
     }
 });
 
